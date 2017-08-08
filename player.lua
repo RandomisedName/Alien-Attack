2df7f1e90cd5c344d00f4d83adf4462dc5878b21
RandomisedName <anatoly.kopyl@gmail.com>
2017-08-08 15:56:17 +0300

    Движение Камеры

--- player.lua
+++ player.lua
@@ -3,6 +3,7 @@ player = {}
 function player.load()
 	player.x = W/2
 	player.y = H*0.2
+	player.screenX = W*0.5
 	player.xVel = 0
 	player.yVel = 0
 	player.speed = 100
@@ -22,9 +23,12 @@ function player.update(dt)
 	-- Трение
 	player.xVel = player.xVel * (1 - math.min(player.friction*dt, 1))
 	player.x = player.x + player.xVel*dt
+	player.screenX = player.screenX + player.xVel*dt
 	player.r = player.xVel/1000
 	
 	player.beaming = love.keyboard.isDown('space')
+	
+	player.screenX = math.min(math.max(player.screenX, W*0.2), W*0.8)
 	
 	function player.keypressed(key)
 		
@@ -35,11 +39,11 @@ function player.draw()
 	if ui.info then
 		love.graphics.setColor(205, 208, 214)
 		love.graphics.setPointSize(4)
-		love.graphics.points(player.x, player.y)
-		love.graphics.line(player.x-15*math.cos(player.r), player.y-15*math.sin(player.r), player.x+15*math.cos(player.r), player.y+15*math.sin(player.r))
+		love.graphics.points(player.screenX, player.y)
+		love.graphics.line(player.screenX-15*math.cos(player.r), player.y-15*math.sin(player.r), player.screenX+15*math.cos(player.r), player.y+15*math.sin(player.r))
 		if player.beaming then
 			love.graphics.setColor(158, 195, 255)
-			love.graphics.line(player.x, player.y, player.x, H)
+			love.graphics.line(player.screenX, player.y, player.screenX, H)
 		end
 	end
 end
